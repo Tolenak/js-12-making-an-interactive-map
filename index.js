2069b207bebf39c1a066736bd0f58e772f5496c1
tyler <@tylerolenak@gmail.com>
2023-09-13 21:06:08 -0700

    applying user location to map

--- index.js
+++ index.js
@@ -54,20 +54,46 @@ const myMap = {
 
 //get coordinates using geolocation
 async function getCoords(){
-	const pos = await new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject)
-	});
-	return [pos.coords.latitude, pos.coords.longitude]
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
+console.log(getCoords());
+
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let data = await response.text()
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+// process foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
 }
 
 
-// get foursquare businesses
-
-
-// process foursquare array
-
-
 // event handlers
+
 // window startup 
  window.onload = async () => {
     let coords = await getCoords ()
